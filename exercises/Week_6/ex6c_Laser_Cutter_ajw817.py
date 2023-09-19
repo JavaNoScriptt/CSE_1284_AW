51d7f4220b2900f4ffbbe91b99582e7b543fa655
eightball889 <56607571+eightball889@users.noreply.github.com>
2023-09-19 18:44:04 -0500

    Completed ex6c_Laser_Cutter_ajw817

--- exercises/Week_6/ex6c_Laser_Cutter_ajw817.py
+++ exercises/Week_6/ex6c_Laser_Cutter_ajw817.py
@@ -1,34 +1,42 @@
 import turtle as turt
 
-s = turt.getscreen()
-t = turt.Turtle()
 turt.title('Hwk 6c Laser Cutter ajw817')
 turt.screensize(800,600)
-
+turt.speed(100)
 # has a list of cordinates for the turtle to move through
-cord_hex =[(-325,275),(-325,175),(-325,75),(-325,-25),(-325,-125),(-225,300),(-225,200),(-225,100),(-225,0),(-225,-100)]
-#different shapes have a different 
-cord_penta = [] 
+cord_hex =[(-325,275),(-325,175),(-325,75),(-325,-25),(-325,-125),(-325,-225),
+           (-225,300),(-225,200),(-225,100),(-225,0),(-225,-100),(-225,-200),
+           (-125,275),(-125,175),(-125,75),(-125,-25),(-125,-125),(-125,-225),
+           (-25,300),(-25,200)]
+#different shapes have a different cordinates
+cord_penta = [(-25,50),(-25,-50),
+              (75,300),(75,200),(75,100),(75,0),(75,-100),(75,-200),
+              (175,300),(175,200),(175,100),(175,0),(175,-100),(175,-200)] 
 
 def teleport(x,y):
-    t.penup()
-    t.goto(x,y)
-    t.pendown()
+    turt.penup()
+    turt.goto(x,y)
+    turt.pendown()
 #This is a way to make shapes with a function
 def draw(sides):
     angle = int(360/sides) # gets the interior angle of the shape
     if sides == 5: # for the different colors
-        t.pencolor('Black')
+        turt.pencolor('Black')
     else:
-        t.pencolor('Blue') 
+        turt.pencolor('Blue') 
     # main part of the program where the sides are drawn
     for i in range(0,sides):
-        t.forward(45) # could make this apart of the function requirements to add more functionalitys
-        t.right(angle)
+        turt.forward(45) # could make this apart of the function requirements to add more functionalitys
+        turt.right(angle)
+
         
 #Draws all of the hexagons
+
 for i in cord_hex:
     teleport(i[0],i[1])
     draw(6)
-    
+
+for i in cord_penta:
+    teleport(i[0],i[1])
+    draw(5)
 
